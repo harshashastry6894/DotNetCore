e2ad8e0178891fdf7303f49179fd4546512daf4b
harshashastry6894 <harshashastry6894@gmail.com>
2020-12-05 22:07:50 +0530

    Adding Get by id method

--- Repository/MockCommanderRepo.cs
+++ Repository/MockCommanderRepo.cs
@@ -1,17 +1,19 @@
 using System.Collections.Generic;
+using System.Linq;
 using MyApp.Models;
 
 namespace MyApp.Repository
 {
     public class MockCommanderRepo : ICommanderRepo
     {
-        // public Command GetCommandById(int id) =>
         private readonly harshaDBContext _context;
 
         public MockCommanderRepo(harshaDBContext context)
         {
             _context = context;
         }
+
+        public Command GetCommandById(int id) => _context.Commands.FirstOrDefault(p => p.Id == id);
 
         public IEnumerable<Command> GetCommands()
         {
